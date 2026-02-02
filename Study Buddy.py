ba59603ac425e073d3b736d647fd19e85752f110
GittohubbonoAkuma <cs1251835@cse.iitd.ac.in>
2026-02-02 10:04:34 +0530

    Update Study Buddy.py

--- Study Buddy.py
+++ Study Buddy.py
@@ -2,135 +2,173 @@ from pdf2image import convert_from_path
 import pytesseract
 import numpy as np
 import cv2
-import tempfile
 import streamlit as st
+import tempfile
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import base64
 
-st.set_page_config(page_title="Study Buddy",layout="wide",page_icon='📘')
+st.set_page_config(page_title="Study Buddy",layout="centered",page_icon='📘')
+
+
+
+
+
 
 @st.cache_resource
 def load_model():
-  return SentenceTransformer("all-MiniLM-L6-v2")
+    return SentenceTransformer("all-MiniLM-L6-v2")
 
 model=load_model()
 
-def pdf_to_img(path,dpi=300):
-  images=convert_from_path(path,dpi=dpi)
-  return images
 
+#
+def pdf_to_img(path,dpi=300):
+    images=convert_from_path(path,dpi=dpi)
+    return images
 
 pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 #process image
+def preprocess_image(pil_image):
+    img=np.array(pil_image)
 
-def preprocess_img(pil_image):
-  img=np.array(pil_image)
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-  gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray=cv2.resize(
+        gray,None,
+        fx=2.0,fy=2.0,
+        interpolation=cv2.INTER_CUBIC
+    )
+    
 
-  gray_img=cv2.resize(
-  gray_img,None,fx=2.0,fy=2.0,interpolation=cv2.INTER_CUBIC)
-
-
-  gray_img=cv2.fastNlMeansDenoising(gray_img,h=10)
-
-  return gray_img
+    gray=cv2.fastNlMeansDenoising(gray,h=10)
 
 
-#Convert PIL image to OpenCV format
+    return gray
+
+
+#convert PIL image to OpenCV format 
 def ocr_image(preprocessed_img):
-  custom_config=r'--oem 3 --psm 6'
-  text=pytesseract.image_to_string(
-  preprocessed_img,
-  lang="eng",
-  config=custom_config)
+    custom_config=r'--oem 3 --psm 6'
+    text=pytesseract.image_to_string(
+        preprocessed_img,
+        lang='eng',
+        config=custom_config
 
-  return text
-def extract_text(path):
-  images=pdf_to_img(path,dpi=300)
-  full_text=""
-  for i,img in enumerate(images):
+    )
+    return text
 
-    processed=preprocess_img(img)
-    page_text=ocr_image(processed)
-    full_text+=page_text+"\n"
 
-  return full_text
+def extract_text_from_handwritten_pdf(path):
+    images=pdf_to_img(path,dpi=300)
+    full_text=""
+
+    for i,img in enumerate(images):
+        
+
+        processed=preprocess_image(img)
+
+        #cv2.imwrite(f"debug_page_{i+1}.png",processed)
+
+        page_text=ocr_image(processed)
+        full_text+=page_text+"\n"
+
+    return full_text
+
 def clean_text(text):
-  text=text.replace("\n"," ")
-  text=" ".join(text.split())
+    text=text.replace("\n"," ")
 
-  return text
+    text=" ".join(text.split())
 
-
+    return text
 
 def chunk_text(text,chunk_size=50):
-  words=text.split()
-  return[
-    " ".join(words[i:i+chunk_size])
-    for i in range(0,len(words),chunk_size)
-  ]
+    words=text.split()
+    return[
+        " ".join(words[i:i+chunk_size])
+        for i in range(0,len(words),chunk_size)
+    ]
 
+
+
+
+#model=SentenceTransformer('all-MiniLM-L6-v2')
 
 def semantic_search(chunks,query):
-  embeddings=model.encode(chunks)
-  query_embeddings=model.encode([query])
+    embeddings=model.encode(chunks)
+    query_embedding=model.encode([query])
 
-  scores=cosine_similarity(query_embeddings,embeddings)
-  best=scores[0].argmax()
+    scores=cosine_similarity(query_embedding,embeddings)
+    best_idx=scores[0].argmax()
 
-  return chunks[best]
+    return chunks[best_idx]
 
-def add_bg(image_path):
-  with open(image_path,"rb") as f:
-    encoded=base64.b64encode(f.read()).decode()
 
-  st.markdown(
-    f"""
-    <style>
-    .stApp{{
-    background-image:url(data:image/jpg;base64,{encoded});
-    background-size:cover;
-    background-repeat:no-repeat;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
 
-  )
 
-#Added my personal bg(lo-fi girl)
+
+def add_bg(image_file):
+    with open(image_file,"rb") as f:
+        encoded=base64.b64encode(f.read()).decode()
+                                 
+    st.markdown(
+        f"""
+        <style>
+        .stApp{{
+        background-image:url(data:image/jpg;base64,{encoded});
+        background-size:cover;
+        background-position:center;
+        background-repeat:no-repeat;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+
+    )    
+        
+
+
+
 add_bg(r"C:\Users\susan\Downloads\lofi girl.jpg")
 
+
+#streamlit UI
 st.title("Study Buddy")
 st.write("Upload your notes and ask your question")
 
 uploaded_file=st.file_uploader("Upload PDF",type=["pdf"])
 
 if uploaded_file is not None:
-  if st.button("Process PDF"):
-    with tempfile.NamedTemporaryFile(delete=False,suffix=".pdf") as tmp:
-      tmp.write(uploaded_file.read())
+    if st.button("Process PDF"):
+        with tempfile.NamedTemporaryFile(delete=False,suffix=".pdf") as tmp:
+            tmp.write(uploaded_file.read())
 
-      pdf_pth=tmp.name
+            pdf_path=tmp.name
 
-    with st.spinner("Running OCR.....Please wait"):
-      raw_text=extract_text(pdf_path)
-      cleaned=clean_text(raw_text)
-      chunks=chunk_text(cleaned)
+        with st.spinner("Running OCR....This may take some time"):
+            raw_text=extract_text_from_handwritten_pdf(pdf_path)
+            cleaned=clean_text(raw_text)
+            chunks=chunk_text(cleaned)
 
 
-  st.session_state["chunks"]=chunks
-  st.success("PDF processed successfully!!")
+        st.session_state["chunks"]=chunks
+
+        st.success("PDF processed successfully!!")
+
 
 
 if "chunks" in st.session_state:
-  query=st.text_input("What do you want to learn?")
+            query=st.text_input("Ask a Question:")
 
-  if query:
-    answer=semantic_search(st.session_state["chunks"],query)
-    st.subheader("Answer")
-    st.write(answer)
-    
+            if query:
+                answer=semantic_search(st.session_state["chunks"],query)
+                st.subheader("Answer")
+                st.write(answer)
+
+
+
+
+
+
+#preprocessing might not work on all docs
